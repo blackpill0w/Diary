aef3bf2604e1c708f4ee7acfaa12610314353ef3
blackpill0w <blackpill0w@protonmail.com>
2022-12-21 10:50:38 +0100

    -

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -6,10 +6,28 @@
 MainWindow::MainWindow(QWidget *parent)
 : QMainWindow(parent)
 {
+   resize(500, 500);
    // Load frame
-   QFile f("../UI/newDiaryFrame.ui");
-   f.open(QIODevice::ReadOnly);
    QUiLoader loader;
-   newDiaryFrame = (QFrame *) loader.load(&f, this);
-   newDiaryFrame->hide();
+
+   QFile f("../UI/initialFrame.ui");
+   f.open(QIODevice::ReadOnly);
+   initial_frame = (QFrame *) loader.load(&f, this);
+   f.close();
+
+   QFile f2("../UI/newDiaryFrame.ui");
+   f2.open(QIODevice::ReadOnly);
+   new_diary_frame = (QFrame *) loader.load(&f2, this);
+   new_diary_frame->hide();
+   f2.close();
+
+   new_diary_button  = initial_frame->findChild<QPushButton*>("newDiaryButton");
+   open_diary_button = initial_frame->findChild<QPushButton*>("openDiaryButton");
+   QObject::connect(new_diary_button, &QPushButton::clicked, [&](){ switch_frame(new_diary_frame); });
+}
+
+void MainWindow::switch_frame(QFrame *frame_to_show) {
+   initial_frame->hide();
+   new_diary_frame->hide();
+   frame_to_show->show();
 }
--- src/main_window.hpp
+++ src/main_window.hpp
@@ -3,10 +3,15 @@
 #include <QMainWindow>
 #include <QWidget>
 #include <QFrame>
+#include <QPushButton>
 
 class MainWindow : public QMainWindow {
 public:
-   QFrame *newDiaryFrame;
+   QFrame *initial_frame;
+   QFrame *new_diary_frame;
+   QPushButton *new_diary_button;
+   QPushButton *open_diary_button;
 public:
    explicit MainWindow(QWidget *parent = nullptr);
+   void switch_frame(QFrame *frame_to_show);
 };
