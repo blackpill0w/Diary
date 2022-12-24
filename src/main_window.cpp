f3e91dd0138ca93f2d1c3f21941af3636a38171d
blackpill0w <blackpill0w@protonmail.com>
2022-12-24 10:08:59 +0100

    Add Creating Diaries

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -1,10 +1,23 @@
 #include "./main_window.hpp"
 
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <cassert>
+
 #include <QUiLoader>
 #include <QFile>
+#include <QDir>
+#include <QLayout>
+#include <QDate>
 
-DiaryApp::DiaryApp() {
+namespace fs = std::filesystem;
+
+DiaryApp::DiaryApp()
+: app_directory_full_path{QDir::homePath().toStdString() + fs::path::preferred_separator + app_directory_name}
+{
+   // Initialize variables
+   diaries.reserve(16);
    // Load frame
    QUiLoader loader;
 
@@ -12,25 +25,53 @@ DiaryApp::DiaryApp() {
    QFile f1("../UI/mainWin.ui");
    f1.open(QIODevice::ReadOnly);
    mainWin = (QMainWindow *) loader.load(&f1);
-   mainWin->resize(600, 600);
-   f1.close();
+   mainWin->resize(winW, winH);
    // Initial frame
    initial_frame = mainWin->findChild<QFrame*>("initialFrame");
+   // Buttons to make a new diary and edit an existing one
    new_diary_button  = initial_frame->findChild<QPushButton*>("newDiaryButton");
    open_diary_button = initial_frame->findChild<QPushButton*>("openDiaryButton");
-   QObject::connect(new_diary_button, &QPushButton::clicked, [&](){ switch_frame(new_diary_frame); });
-   QObject::connect(open_diary_button, &QPushButton::clicked, [&](){ switch_frame(new_diary_frame); });
+   // Add events
+   QObject::connect(open_diary_button, &QPushButton::clicked, [&](){ switch_frame(load_diary_frame); load_diaries(); });
+   QObject::connect(new_diary_button, &QPushButton::clicked, [&]() {
+      diary_title_entry->clear();
+      diary_body_entry->clear();
+      switch_frame(edit_diary_frame);
+   });
+
    // New diary (the editor) frame
    QFile f2("../UI/newDiaryFrame.ui");
    f2.open(QIODevice::ReadOnly);
-   new_diary_frame = (QFrame *) loader.load(&f2, mainWin);
-   new_diary_frame->hide();
-   f2.close();
+   edit_diary_frame = (QFrame *) loader.load(&f2, mainWin);
+   edit_diary_frame->hide();
+   // Diary title line edit
+   diary_title_entry = edit_diary_frame->findChild<QLineEdit*>("diaryTitleLineEdit");
+   // Diary body text edit
+   diary_body_entry = edit_diary_frame->findChild<QTextEdit*>("diaryBodyTextEdit");
+   // Save button
+   save_button = edit_diary_frame->findChild<QPushButton*>("saveButton");
+   QObject::connect(save_button, &QPushButton::clicked, [&](){ save_diary(); });
    // Button to go back to the initial frame
-   go_back_button  = new_diary_frame->findChild<QPushButton*>("goBackButton");
-   go_back_button->setIcon(QIcon("../img/left_arrow.png"));
-   go_back_button->setIconSize(QSize(42, 25));
-   QObject::connect(go_back_button, &QPushButton::clicked, [&](){ switch_frame(initial_frame); });
+   EDF_go_back_button = edit_diary_frame->findChild<QPushButton*>("goBackButton");
+   // Add icon
+   EDF_go_back_button->setIcon(QIcon("../img/left_arrow.png"));
+   EDF_go_back_button->setIconSize(QSize(42, 25));
+   // Add event
+   QObject::connect(EDF_go_back_button, &QPushButton::clicked, [&](){ switch_frame(initial_frame); });
+
+   // Load diary frame
+   QFile f3("../UI/loadDiaryFrame.ui");
+   f3.open(QIODevice::ReadOnly);
+   load_diary_frame = (QFrame *) loader.load(&f3, mainWin);
+   load_diary_frame->hide();
+   diaries_buttons_widget = load_diary_frame->findChild<QWidget*>("diariesButtonsWidget");
+   // Button to go back to the initial frame
+   LDF_go_back_button = load_diary_frame->findChild<QPushButton*>("goBackButton");
+   // Add icon
+   LDF_go_back_button->setIcon(QIcon("../img/left_arrow.png"));
+   LDF_go_back_button->setIconSize(QSize(42, 25));
+   // Add event
+   QObject::connect(LDF_go_back_button, &QPushButton::clicked, [&](){ switch_frame(initial_frame); });
 }
 
 void DiaryApp::start() {
@@ -38,7 +79,51 @@ void DiaryApp::start() {
 }
 
 void DiaryApp::switch_frame(QFrame *frame_to_show) {
+   // Switch to a certain frame
    initial_frame->hide();
-   new_diary_frame->hide();
+   edit_diary_frame->hide();
+   load_diary_frame->hide();
    frame_to_show->show();
+}
+
+void DiaryApp::save_diary() {
+   assert(!edit_diary_frame.isHidden());
+   const std::string file_path {
+      app_directory_full_path
+      + fs::path::preferred_separator
+      + QDate::currentDate().toString("dd-MM-yyyy").toStdString()
+      + ".txt"
+   };
+   std::ofstream diary_file{ file_path };
+   diary_file << diary_title_entry->text().toStdString()
+              << '\n'
+              << diary_body_entry->toPlainText().toStdString();
+}
+
+void DiaryApp::load_diary(QString file_path) {
+//   assert(!edit_diary_frame.isHidden());
+   std::cout << file_path.toStdString() << std::endl;
+
+}
+
+void DiaryApp::load_diaries() {
+   assert(!load_diary_frame.isHidden());
+   // Add a button for each diary file
+   // Each button opens its correspinding file to edit
+   for (const auto& diary_file: fs::directory_iterator(app_directory_full_path)) {
+      QPushButton *b = new QPushButton();
+      const QString file_path = std::string(diary_file.path()).c_str();
+      b->setText(file_path);
+      diaries_buttons_widget->layout()->addWidget(b);
+      QObject::connect(
+         b,
+         &QPushButton::clicked,
+         [&](){ load_diary(file_path); }
+      );
+
+      b->show();
+      diaries.emplace_back(b);
+   }
+   // Push everything to the top
+   diaries_buttons_widget->layout()->setAlignment(Qt::AlignTop);
 }
--- src/main_window.hpp
+++ src/main_window.hpp
@@ -1,21 +1,49 @@
 #pragma once
 
+#include <string>
+#include <vector>
+#include <memory>
+
 #include <QMainWindow>
 #include <QWidget>
 #include <QFrame>
 #include <QPushButton>
+#include <QLineEdit>
+#include <QTextEdit>
 
 class DiaryApp {
+public: // static vars
+   inline static constexpr int winW = 600;
+   inline static constexpr int winH = 600;
+   inline static const std::string app_directory_name = ".diary";
 public:
-   QMainWindow *mainWin;
-   QFrame *initial_frame;
-   QFrame *new_diary_frame;
-   QPushButton *new_diary_button;
-   QPushButton *open_diary_button;
-   // Inside new diary frame
-   QPushButton *go_back_button;
+   const std::string app_directory_full_path;
+private:
+   // Main window
+   QMainWindow  *mainWin                = nullptr;
+   // Frames
+   QFrame       *initial_frame          = nullptr;
+   QFrame       *edit_diary_frame       = nullptr;
+   QFrame       *load_diary_frame       = nullptr;
+   // Inside initial_frame
+   QPushButton  *new_diary_button       = nullptr;
+   QPushButton  *open_diary_button      = nullptr;
+   // Inside edit_diary_frame
+   QPushButton  *EDF_go_back_button     = nullptr;
+   QPushButton  *save_button            = nullptr;
+   QLineEdit    *diary_title_entry      = nullptr;
+   QTextEdit    *diary_body_entry       = nullptr;
+   // Inside load_diary_frame
+   QPushButton  *LDF_go_back_button     = nullptr;
+   QWidget      *diaries_buttons_widget = nullptr;
+   std::vector< std::unique_ptr<QPushButton> > diaries{};
+
 public:
    explicit DiaryApp();
-   void switch_frame(QFrame *frame_to_show);
    void start();
+private:
+   void switch_frame(QFrame *frame_to_show);
+   void save_diary();
+   void load_diary(QString file_path);
+   void load_diaries();
 };
